760d7a1b3d3795b8fc6bc4f542e577a53706cc8d
Isomorphismss <jw6699@nyu.edu>
2023-11-22 00:17:01 -0500

    added function to receive audio file and store history in database

---
+++ machine-learning-client/app.py
@@ -0,0 +1,38 @@
+"""Does something interesting."""
+
+from flask import Flask, request, jsonify
+import pymongo
+from ml_client import transcribe_audio, analyze_sentiment
+
+app = Flask(__name__)
+
+client = pymongo.MongoClient("mongodb://admin:secret@localhost:27017")
+db = client["Isomorphism"]
+collection = db["history"]
+app.config["SECRET_KEY"] = "supersecretkey"
+
+
+@app.route("/upload", methods=["POST"])
+def upload_audio():
+    """Does something interesting."""
+    if "audio" not in request.files:
+        return "No audio file", 400
+
+    audio_file = request.files["audio"]
+    audio_path = "file.wav"
+    audio_file.save(audio_path)
+
+    transcript = transcribe_audio(audio_path)
+    sentiment = analyze_sentiment(transcript)
+
+    document = {
+        "transcript": transcript,
+        "sentiment": sentiment.polarity,
+    }
+    collection.insert_one(document)
+
+    return jsonify({"transcript": transcript, "sentiment": sentiment}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
