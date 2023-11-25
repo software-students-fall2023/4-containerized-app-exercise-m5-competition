d96784e6cf62186c6bf0c0c56523e96698a83d61
fzfzlfz <101171656+fzfzlfz@users.noreply.github.com>
2023-11-24 22:31:46 -0800

    make web looks more readers friendly

--- web-app/app.py
+++ web-app/app.py
@@ -3,6 +3,7 @@
 import os
 import requests
 import pymongo
+import json
 from flask import Flask, request, render_template
 
 
@@ -28,7 +29,17 @@ def upload_audio():
     response = requests.post(
         "http://mlclient:5000/upload", files={"audio": audio_file}, timeout=5
     )
-    return response.content, response.status_code
+    # Decoding the byte string to a regular string
+    response_text = response.content.decode('utf-8')
+
+    # Parsing the JSON (optional, if you want to extract specific data)
+    try:
+        response_data = json.loads(response_text)
+    except json.JSONDecodeError:
+        # Handle error if response is not valid JSON
+        response_data = {"error": "Invalid JSON response"}
+        
+    return render_template('result.html', response_content=response_data, status_code=response.status_code)
 
 
 if __name__ == "__main__":
