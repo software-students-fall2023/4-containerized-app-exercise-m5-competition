d29da881b845ed23d597b1670ce893e28f9b4731
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-25 15:21:32 -0500

    login utility

--- web-app/app.py
+++ web-app/app.py
@@ -1,7 +1,6 @@
 """place-holder"""
 
 import os
-import json
 import requests
 import pymongo
 from flask import Flask, request, render_template
@@ -29,19 +28,7 @@ def upload_audio():
     response = requests.post(
         "http://mlclient:5000/upload", files={"audio": audio_file}, timeout=5
     )
-    # Decoding the byte string to a regular string
-    response_text = response.content.decode("utf-8")
-
-    # Parsing the JSON (optional, if you want to extract specific data)
-    try:
-        response_data = json.loads(response_text)
-    except json.JSONDecodeError:
-        # Handle error if response is not valid JSON
-        response_data = {"error": "Invalid JSON response"}
-
-    return render_template(
-        "result.html", response_content=response_data, status_code=response.status_code
-    )
+    return response.content, response.status_code
 
 
 if __name__ == "__main__":
--- web-app/user/models.py
+++ web-app/user/models.py
@@ -1,3 +1,51 @@
 from flask import Flask, jsonify, request, session, redirect
 from passlib.hash import pbkdf2_sha256
+from app import db
 
+class User:
+    """Utility functions for user"""
+
+    def start_session(self, user):
+        """Create session containing user info"""
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+
+    def signup(self):
+        """signing up"""
+        print(request.form)
+
+        # Create the user object
+        user = {
+          "username": request.form.get('username'),
+          "password": request.form.get('password')
+        }
+
+        # Encrypt the password
+        user['password'] = pbkdf2_sha256.encrypt(user['password'])
+
+        # Check for existing username address
+        if db.users.find_one({ "usernmae": user['username'] }):
+            return jsonify({ "error": "Username already in use" }), 400
+
+        if db.users.insert_one(user):
+            return self.start_session(user)
+
+        return jsonify({ "error": "Signup failed" }), 400
+
+    def signout(self):
+        """signing out"""
+        session.clear()
+        return redirect('/')
+
+    def login(self):
+        """logging in """
+        user = db.users.find_one({
+            "username": request.form.get('username')
+        })
+
+        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+            return self.start_session(user)
+
+        return jsonify({ "error": "Invalid login credentials" }), 401
