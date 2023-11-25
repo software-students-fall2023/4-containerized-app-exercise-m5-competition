e772b92911296f6c7d0fa4afacbda1474d5b5712
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-25 15:34:17 -0500

    add login required decorator and placeholder view

--- web-app/app.py
+++ web-app/app.py
@@ -1,23 +1,40 @@
 """place-holder"""
-
+from functools import wraps
 import os
 import requests
 import pymongo
-from flask import Flask, request, render_template
-
+from flask import Flask, request, render_template, redirect, session
 
 app = Flask(__name__)
-
 # Connecting to local host and same db as ml's backend
 client = pymongo.MongoClient("mongodb://db:27017")
 db = client["Isomorphism"]
 
+# Login require decorators
+def login_required(f):
+    """login required decorators"""
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+
+    return wrap
 
 # Views
 @app.route("/")
 def homescreen_view():
     """upload audio"""
     return render_template("index.html")
+
+
+@app.route("/transcripts")
+@login_required
+def transcripts_view():
+    """View transcripts generated before by the user"""
+    return render_template("transcripts.html")
+
 
 
 # Form handlers
