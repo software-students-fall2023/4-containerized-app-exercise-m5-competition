fbe5157e1d30ae3dbc1cdbc38c3c1037c06c976c
fzfzlfz <101171656+fzfzlfz@users.noreply.github.com>
2023-11-26 18:09:18 -0800

    fix bugs to pass pytest

--- web-app/app.py
+++ web-app/app.py
@@ -157,4 +157,4 @@ def test_mic():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5001")))
