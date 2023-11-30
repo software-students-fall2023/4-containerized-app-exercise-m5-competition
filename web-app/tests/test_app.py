904f915a61675a63e65c17344b9c223e6f9308da
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-30 16:05:27 -0500

    lint all fiews

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 from io import BytesIO
 from unittest.mock import patch
 
+
 # Setup and Teardown
 @pytest.fixture
 def app():
@@ -11,31 +12,38 @@ def app():
     flask_app.config["TESTING"] = True
     yield flask_app
 
+
 @pytest.fixture
 def client(app):
     return app.test_client()
 
+
 # User Authentication Tests
 def test_user_signup(client):
-    response = client.post('/user/signup', data={
-        'username': 'testuser',
-        'password': 'testpassword'
-    })
+    response = client.post(
+        "/user/signup", data={"username": "testuser", "password": "testpassword"}
+    )
     assert response.status_code == 200
     # Add more assertions to validate response content, session, etc.
 
+
 # Audio Upload and Processing Tests
 def test_audio_upload(client):
-    with patch('requests.post') as mock_post:
+    with patch("requests.post") as mock_post:
         mock_post.return_value.status_code = 200
-        mock_post.return_value.json.return_value = {'transcription': 'test transcription'}
+        mock_post.return_value.json.return_value = {
+            "transcription": "test transcription"
+        }
 
-        response = client.post('/api/upload_audio', data={
-            'audio': (BytesIO(b'test audio data'), 'test_audio.wav')
-        }, content_type='multipart/form-data')
+        response = client.post(
+            "/api/upload_audio",
+            data={"audio": (BytesIO(b"test audio data"), "test_audio.wav")},
+            content_type="multipart/form-data",
+        )
 
         assert response.status_code == 200
-        assert 'test transcription' in response.data.decode()
+        assert "test transcription" in response.data.decode()
+
 
 # Additional tests can be added here...
 
