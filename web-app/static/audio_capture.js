13c2ec3fec1af374e409437883b54516235f094c
Isomorphismss <jw6699@nyu.edu>
2023-12-01 23:39:23 -0500

    final style change

--- web-app/static/audio_capture.js
+++ web-app/static/audio_capture.js
@@ -3,23 +3,34 @@ let audioChunks = [];
 
 document.getElementById("start-recording").addEventListener("click", async () => {
     console.log("Start recording button clicked");
+    document.getElementById("start-recording").disabled = true;
+    document.getElementById("stop-recording").disabled = false;
+
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     mediaRecorder = new MediaRecorder(stream);
     audioChunks = [];
+
     mediaRecorder.ondataavailable = e => audioChunks.push(e.data);
     mediaRecorder.start();
-    document.getElementById("stop-recording").disabled = false;
 
-    document.getElementById("stop-recording").addEventListener("click", () => {
-        console.log("Stop recording button clicked");
-        mediaRecorder.stop();
-        document.getElementById("stop-recording").disabled = true;
-        mediaRecorder.onstop = () => {
-            const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
-            sendAudioToServer(audioBlob);
-            console.log("Recording stopped");
-        };
-    });
+    // Update UI to show recording status
+    document.getElementById("response-message").innerHTML = "<p class='recording-status'>Recording in progress...</p>";
+});
+
+document.getElementById("stop-recording").addEventListener("click", () => {
+    console.log("Stop recording button clicked");
+    mediaRecorder.stop();
+    document.getElementById("stop-recording").disabled = true;
+    document.getElementById("start-recording").disabled = false;
+
+    // Update UI to show recording has stopped
+    document.getElementById("response-message").innerHTML = "<p class='recording-status'>Recording stopped. Processing audio...</p>";
+
+    mediaRecorder.onstop = () => {
+        const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+        sendAudioToServer(audioBlob);
+        console.log("Recording stopped");
+    };
 });
 
 function sendAudioToServer(audioBlob) {
@@ -50,18 +61,43 @@ function sendAudioToServer(audioBlob) {
 
 function updateResultPage(data) {
     const resultContainer = document.getElementById("response-message");
-    resultContainer.innerHTML = `
+    let htmlContent = `
         <div>
             <h3>Transcribed Text:</h3>
-            <p>${data.transcript || 'No transcription available.'}</p>
-        </div>
-        <div>
-            <h3>Sentiment Analysis:</h3>
-            <p>${data.sentiment || 'No sentiment data.'}</p>
+            <p>${data.transcript}</p>
         </div>
     `;
+
+    // Check if sentiment is -2 and display 'N/A' if true
+    if (data.sentiment === -2) {
+        htmlContent += `
+            <div>
+                <h3>Sentiment Polarity:</h3>
+                <p>N/A</p>
+            </div>
+        `;
+    } else if (data.sentiment >= -1 && data.sentiment <= 1) {
+        htmlContent += `
+            <div>
+                <h3>Sentiment Polarity:</h3>
+                <p>${data.sentiment}</p>
+            </div>
+        `;
+        let sentimentInterpretation = "Sentiment Interpretation: Neutral"; // default
+        if (data.sentiment > 0.5) {
+            sentimentInterpretation = "Sentiment Interpretation: Strongly Positive";
+        } else if (data.sentiment > 0) {
+            sentimentInterpretation = "Sentiment Interpretation: Slightly Positive";
+        } else if (data.sentiment < -0.5) {
+            sentimentInterpretation = "Sentiment Interpretation: Strongly Negative";
+        } else if (data.sentiment < 0) {
+            sentimentInterpretation = "Sentiment Interpretation: Slightly Negative";
+        }
+        htmlContent += `<p class="sentiment-interpretation">${sentimentInterpretation}</p>`;
+    }
+
     if (data.filename) {
-        resultContainer.innerHTML += `
+        htmlContent += `
             <div>
                 <h3>Listen to the Audio:</h3>
                 <audio controls>
@@ -71,4 +107,12 @@ function updateResultPage(data) {
             </div>
         `;
     }
+
+    resultContainer.innerHTML = htmlContent;
 }
+
+document.getElementById('audio-file').addEventListener('change', function() {
+    // This code will run when the file input changes
+    var fileName = this.files[0].name; // Gets the file name
+    document.getElementById('file-chosen').textContent = fileName; // Updates the span text
+});
